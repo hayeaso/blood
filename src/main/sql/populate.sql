72dd48df110496a0312b39ee1957f023bc4c09e3
haye <haye.sidi@gmail.com>
2017-02-06 18:15:54 -0600

    populate added

---
+++ src/main/sql/populate.sql
@@ -0,0 +1,6 @@
+INSERT INTO `lifesavers`.`address` (`city`, `state`, `street`) VALUES ('fairfield', 'IA', '1000 N 4th ST');
+INSERT INTO `lifesavers`.`address` (`city`, `state`, `street`) VALUES ('fairfield', 'IA', '1705 W Burlington Ave');
+INSERT INTO `lifesavers`.`eligibility` (`donationDate`, `eligible`) VALUES ('2017-02-22 00:00:00', '0');
+INSERT INTO `lifesavers`.`donor` (`bloodGroup`, `email`, `firstName`, `lastName`, `password`, `registrationDate`, `username`, `Address`, `Eligible`) VALUES ('O_positive', 'haye.sidi@gmail.com', 'Haye', 'Haye', '123456', '2017-02-22 00:00:00', 'hayuso', '1', '1');
+INSERT INTO `lifesavers`.`donor` (`bloodGroup`, `email`, `firstName`, `lastName`, `password`, `registrationDate`, `username`, `Address`, `Eligible`) VALUES ('O_positive', 'test@tt.com', 'test', 'test', 'test', '2017-02-22 00:00:00', 'test', '1', '1');
+INSERT INTO `lifesavers`.`donor` (`bloodGroup`, `email`, `firstName`, `lastName`, `password`, `registrationDate`, `username`, `Address`, `Eligible`) VALUES ('O_negative', 'test@tt.com', 'user', 'user', 'user', '2017-02-22 00:00:00', 'user', '2', '1');
